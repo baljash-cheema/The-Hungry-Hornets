0f91711be0ac2dc8aecc8794ab9a106e697dfe26
lbarsky <88569183+lbarsky@users.noreply.github.com>
2021-11-02 14:38:45 -0500

    Add files via upload
    coding for all boolean type conversions requested

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,11 +1,21 @@
 import pandas as pd
 
-if __name__ == '__main__':
-    file = 'csv/postgres_public_trr_trr_refresh.csv'
-    df = pd.read_csv(file)
+df = pd.read_csv('postgres_public_trr_trr_refresh.csv')
 
-    pd.to_datetime(df['trr_datetime'])
+#for x in df['officer_on_duty']:
+    #if x == "Yes" or x == "Y":
+        #x.replace = True
 
-    df["officer_on_duty"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
-    print(df["officer_on_duty"])
-    print(df["officer_on_duty"].unique())
+df["officer_on_duty"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["officer_injured"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["officer_in_uniform"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["subject_armed"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["subject_injured"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["subject_alleged_injury"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["notify_oemc"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["notify_district_sergeant"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["notify_op_command"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["notify_det_division"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+
+df["firearm_reloaded"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+df["sight_used"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
