3f807d5a8f39930de1b2852846c6008e2da9ea24
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-02 14:24:13 -0500

    updates

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,10 +1,8 @@
 import pandas as pd
 
-df = pd.read_csv('postgres_public_trr_trr_refresh.csv')
+file = 'postgres_public_trr_trr_refresh.csv'
 
-#for x in df['officer_on_duty']:
-    #if x == "Yes" or x == "Y":
-        #x.replace = True
+df_trr_trr = pd.read_csv(file)
 
 df["officer_on_duty"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
 print df["officer_on_duty"]
