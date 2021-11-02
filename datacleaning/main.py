1af3c0aa7e1d9db0e8ea8604e1118eeabf8bf0b5
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-02 14:36:09 -0500

    updates

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,9 +1,11 @@
 import pandas as pd
 
-file = 'postgres_public_trr_trr_refresh.csv'
+if __name__ == '__main__':
+    file = 'csv/postgres_public_trr_trr_refresh.csv'
+    df = pd.read_csv(file)
 
-df_trr_trr = pd.read_csv(file)
+    pd.to_datetime(df['trr_datetime'])
 
-df["officer_on_duty"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
-print df["officer_on_duty"]
-print df["officer_on_duty"].unique()
+    df["officer_on_duty"].replace({"Yes": True, "Y": True, "No": False, "N": False}, inplace=True)
+    print(df["officer_on_duty"])
+    print(df["officer_on_duty"].unique())
