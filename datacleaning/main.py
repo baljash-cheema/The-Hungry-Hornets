650320fb66334a94ef0cf759dcee2cfcb6a16cf3
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-04 13:23:25 -0500

    update code

--- datacleaning/main.py
+++ datacleaning/main.py
@@ -1,3 +1,5 @@
+import csv
+
 import pandas as pd
 
 def convert_bool(df,list):
@@ -71,23 +73,60 @@ def typecorrection():
 # need to convert to csv
 # still with time issue
 
-if __name__ == '__main__':
-    typecorrection()
+def integration():
+    trr_refresh = 'csv/postgres_public_trr_trr_refresh.csv'
+    data_officer = 'csv/postgres_public_data_officer.csv'
+    office_list=[]
+    refresh_list=[]
+    with open(trr_refresh) as csvfile:
+        refresh_reader = csv.DictReader(csvfile)
 
+        with open(data_officer) as csvfile2:
+            officer_reader = csv.DictReader(csvfile2)
+
+            count = 0
+
+            for row in refresh_reader:
+                refresh_tuple = tuple()
+                year = row['officer_birth_year'][0:4]
+                last_name = row['officer_last_name'].lower()
+                refresh_tuple = (row['officer_last_name'].lower().strip() ,row['officer_first_name'].lower().strip())
+                # print('refresh tuple',refresh_tuple)
+
+            for all in officer_reader:
+                officer_tuple = tuple()
+                officer_tuple = (all['last_name'].lower().strip(), all['first_name'].lower().strip())
+                #print('officer_tuple',officer_tuple)
+                #print('refresh_tuple',refresh_tuple)
+            office_list.append(list(officer_tuple))
+            refresh_list.append(list(refresh_tuple))
+
+    for x in office_list:
+        for y in refresh_list:
+            if x==y:
+                print(x)
+
+
+
+
+
+
+    # for x in range(0, len(df1)):
+    #     for y in range(0, len(df2)):
+    #         if (df1['officer_first_name'][x] == df2['officer_first_name'][y] and df1['officer_middle_initial'][x] ==
+    #                 df2['officer_middle_initial'][y]
+    #                 and df1['officer_last_name'][x] == df2['officer_last_name'][y] and df1['officer_age'][x] ==
+    #                 df2['officer_age'][y] and
+    #                 df1['officer_race'][x] == df2['officer_race'][y]):
+    #             new_id = df3['id']  # data_officer id corresponding to given first and last name?
+    #             new_officer_id.append(df3['id'])  # generate column of ids which we can then merge with trr_trr_refresh
+
+if __name__ == '__main__':
+    # typecorrection()
+    integration()
 #Lili starter code
 #For creating a new column of officer ids to link the TRR filing officers with the TRR updating officers:
 
-df1 = pd.read_csv('csv/postgres_public_trr_trr_refresh.csv')
-df2 = pd.read_csv('csv/postgres_public_trr_trrstatus_refresh.csv')
-df3 = pd.read_csv('csv/postgres_public_data_officer.csv')
 
-new_officer_id=[]
-for x in range(0,len(df1)):
-  for y in range(0, len(df2)):
-    if (df1['officer_first_name'][x]==df2['officer_first_name'][y] and df1['officer_middle_initial'][x]==df2['officer_middle_initial'][y]
-        and df1['officer_last_name'][x]==df2['officer_last_name'][y] and df1['officer_age'][x]==df2['officer_age'][y] and
-        df1['officer_race'][x]==df2['officer_race'][y]):
-        new_id = df3['id'] #data_officer id corresponding to given first and last name?
-        new_officer_id.append(df3['id']) #generate column of ids which we can then merge with trr_trr_refresh
 
 
