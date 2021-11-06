cb8b15505d64dcc830410d07e9f0d37d8f3591cd
vikramNU <90998121+vikramNU@users.noreply.github.com>
2021-11-06 18:13:40 -0500

    Update main.py

--- datacleaning/src/main.py
+++ datacleaning/src/main.py
@@ -235,7 +235,7 @@ def reconciliation(List):
       df['officer_race'] = df['officer_race'].replace([x],y)
 
     df.to_csv('csv/after_recon/trr_status_0.csv')
-    
+
 def redact(List):
 
     df1 = pd.read_csv(List[0])
@@ -264,11 +264,11 @@ def redact(List):
     df1.to_csv('src/csv/after_redact/postgres_public_trr_trr_refresh.csv')
     df2.to_csv('src/csv/after_redact/postgres_public_trr_weapondischarge_refresh.csv')
     df3.to_csv('src/csv/after_redact/postgres_public_trr_trrstatus_refresh.csv')
-
+    
 def integration(List):
     trr_df = pd.read_csv(List[0])
     officer_df = pd.read_csv(List[1])
-    
+
     trr_df['officer_appointed_date'].replace({'REDACTED': None},inplace =True)
     trr_df['officer_appointed_date']= pd.to_datetime(trr_df['officer_appointed_date']).dt.date
 
@@ -279,9 +279,9 @@ def integration(List):
     df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
 
     df1 = pd.merge(df4, officer_df, how='left',
-                      left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_appointed_date', 'officer_birth_year', 'officer_gender'],
-                      right_on=['first_name', 'race', 'last_name', 'appointed_date', 'birth_year',
-                                'gender']).fillna("None")
+                       left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_appointed_date', 'officer_birth_year', 'officer_gender'],
+                       right_on=['first_name', 'race', 'last_name', 'appointed_date', 'birth_year',
+                                 'gender']).fillna("None")
 
     df6 = df1[df1['id'] != 'None'].reset_index(drop=True)
     df4 = df1[df1['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
@@ -289,25 +289,109 @@ def integration(List):
     df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
 
     df2 = pd.merge(df4, officer_df, how='left',
-                      left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_birth_year', 'officer_gender'],
-                      right_on=['first_name', 'race', 'last_name', 'birth_year', 'gender']).fillna("None")
-    print(df2.columns)
+                       left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_birth_year', 'officer_gender'],
+                       right_on=['first_name', 'race', 'last_name', 'birth_year', 'gender']).fillna("None")
     p3 = df2[df2['id'] != 'None'].reset_index(drop=True)
     no_p3 = df2[df2['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
 
     no_p3['officer_birth_year'] = no_p3['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
 
     df3 = pd.merge(no_p3, officer_df, how='left', left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_gender'],
-                      right_on=['first_name', 'race', 'last_name', 'gender']).fillna("None")
+                       right_on=['first_name', 'race', 'last_name', 'gender']).fillna("None")
 
     p4 = df3[df3['id'] != 'None'].reset_index(drop=True)
     p5 = df3[df3['id'] == 'None'].reset_index(drop=True)
 
     final_trr_cleaned = pd.concat([df6, df6, p3, p4, p5])
+    print(final_trr_cleaned.columns)
     final_trr_status_cleaned = final_trr_cleaned[['officer_rank', 'officer_star', 'status', 'status_datetime', 'officer_age', 'officer_unit_at_incident',
-        'trr_report_id','id']]
+         'trr_report_id','id']]
 
     final_trr_status_cleaned.to_csv('merge2.csv')
+
+    final_trr_cleaned = final_trr_cleaned.reset_index(drop=True)
+    #final_trr_cleaned.to_csv('merge2.csv')
+
+    trr_df = pd.read_csv(List[0])
+    officer_df = pd.read_csv(List[1])
+    trr_df = trr_df.rename(columns={'id': 'trr_id'})
+
+    trr_df['officer_appointed_date'].replace({'REDACTED': None}, inplace=True)
+    #'officer_first_name', 'officer_middle_initial', 'officer_last_name', 'officer_middle_initial', 'officer_appointed_date', 'officer_birth_year', 'officer_gender', 'officer_race'],
+    #'first_name','middle_initial','last_name','suffix_name','appointed_date','birth_year','gender','race'])
+    df = pd.merge(trr_df, officer_df, how='left',
+                  left_on=['officer_first_name', 'officer_middle_initial','officer_last_name','officer_appointed_date', 'officer_gender'
+                           ],
+                  right_on=['first_name', 'middle_initial','last_name','appointed_date','gender']).fillna("None")
+    df9 = df[df['id'] != 'None'].reset_index(drop=True)
+    df4 = df[df['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
+    df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
+
+    df1 = pd.merge(df4, officer_df, how='left',
+                   left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_appointed_date',
+                            'officer_birth_year', 'officer_gender'],
+                   right_on=['first_name', 'race', 'last_name', 'appointed_date', 'birth_year',
+                             'gender']).fillna("None")
+
+    df6 = df1[df1['id'] != 'None'].reset_index(drop=True)
+    df4 = df1[df1['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
+
+    df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
+
+    df2 = pd.merge(df4, officer_df, how='left',
+                   left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_birth_year',
+                            'officer_gender'],
+                   right_on=['first_name', 'race', 'last_name', 'birth_year', 'gender']).fillna("None")
+    p3 = df2[df2['id'] != 'None'].reset_index(drop=True)
+    no_p3 = df2[df2['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
+
+    no_p3['officer_birth_year'] = no_p3['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
+
+    df3 = pd.merge(no_p3, officer_df, how='left',
+                   left_on=['officer_first_name', 'officer_race', 'officer_last_name', 'officer_gender'],
+                   right_on=['first_name', 'race', 'last_name', 'gender']).fillna("None")
+
+    p4 = df3[df3['id'] != 'None'].reset_index(drop=True)
+    p5 = df3[df3['id'] == 'None'].reset_index(drop=True)
+
+    final_trr_cleaned = pd.concat([df9, df6, p3, p4, p5])
+    final_trr_cleaned = final_trr_cleaned[['trr_id', 'event_number', 'cr_number', 'beat', 'block', 'direction',
+                                           'street', 'location', 'trr_datetime',
+                                           'indoor_or_outdoor', 'lighting_condition', 'weather_condition',
+                                           'notify_oemc', 'notify_district_sergeant', 'notify_op_command',
+                                           'notify_det_division', 'party_fired_first', 'officer_assigned_beat',
+                                           'officer_on_duty', 'officer_in_uniform', 'officer_injured',
+                                           'officer_rank', 'subject_armed', 'subject_injured',
+                                           'subject_alleged_injury', 'subject_age', 'subject_birth_year',
+                                           'subject_gender', 'subject_race', 'officer_age',
+                                           'officer_unit_name', 'officer_unit_detail',
+                                           'trr_created', 'latitude', 'longitude', 'point']]
+    final_trr_cleaned = final_trr_cleaned.reset_index(drop=True)
+    for i in range(len(final_trr_cleaned['officer_unit_name'])):
+        if len(final_trr_cleaned['officer_unit_name'][i]) == 3:
+            final_trr_cleaned['officer_unit_name'][i] = final_trr_cleaned['officer_unit_name'][i]
+        elif len(final_trr_cleaned['officer_unit_name'][i]) == 2:
+            final_trr_cleaned['officer_unit_name'][i] = '0' + final_trr_cleaned['officer_unit_name'][i]
+        elif len(final_trr_cleaned['officer_unit_name'][i]) == 1:
+            final_trr_cleaned['officer_unit_name'][i] = '00' + final_trr_cleaned['officer_unit_name'][i]
+        print(final_trr_cleaned['officer_unit_name'][i])
+    unit_info=pd.read_csv('csv/original/postgres_public_data_policeunit.csv')
+
+    new_df_unit_id = pd.merge(final_trr_cleaned, unit_info, how='left', left_on='officer_unit_name',
+                              right_on='unit_name').fillna("None")
+
+    new_df_unit_id = new_df_unit_id[['trr_id', 'event_number', 'cr_number', 'beat', 'block', 'direction',
+                                     'street', 'location', 'trr_datetime', 'indoor_or_outdoor',
+                                     'lighting_condition', 'weather_condition', 'notify_oemc',
+                                     'notify_district_sergeant', 'notify_op_command', 'notify_det_division',
+                                     'party_fired_first', 'officer_assigned_beat', 'officer_on_duty',
+                                     'officer_in_uniform', 'officer_injured', 'officer_rank',
+                                     'subject_armed', 'subject_injured', 'subject_alleged_injury',
+                                     'subject_age', 'subject_birth_year', 'subject_gender', 'subject_race',
+                                     'officer_age', 'officer_unit_name', 'officer_unit_detail',
+                                     'trr_created', 'latitude', 'longitude', 'point', 'id', 'description']]
+    new_df_unit_id = new_df_unit_id.rename(columns={'id': 'officer_unit_id', 'description': 'officer_unit_detail_id'})
+    new_df_unit_id.to_csv('merge5.csv')
 
 if __name__ == '__main__':
     #Type correct after OpenRefine
