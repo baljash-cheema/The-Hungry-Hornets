8a0ccd5b2d712cd08f7a58b42e1266508c27a5fa
vikramNU <90998121+vikramNU@users.noreply.github.com>
2021-11-06 19:38:26 -0500

    Update main.py

--- datacleaning/src/main.py
+++ datacleaning/src/main.py
@@ -266,16 +266,16 @@ def redact(List):
     df3.to_csv('src/csv/after_redact/postgres_public_trr_trrstatus_refresh.csv')
 
 def integration(List):
-    #3.1 trr_status starts here
     trr_df = pd.read_csv(List[2])
     officer_df = pd.read_csv(List[1])
 
-    trr_df['officer_appointed_date'].replace({'REDACTED': None},inplace =True)
-    trr_df['officer_appointed_date']= pd.to_datetime(trr_df['officer_appointed_date']).dt.date
 
-    df = pd.merge(trr_df, officer_df,  how='left', left_on=['officer_first_name','officer_middle_initial','officer_last_name','officer_middle_initial','officer_appointed_date','officer_birth_year','officer_gender','officer_race'],
-                  right_on = ['first_name','middle_initial','last_name','suffix_name','appointed_date','birth_year','gender','race']).fillna("None")
-    df6 = df[df['id'] != 'None'].reset_index(drop=True)
+    trr_df['officer_appointed_date'].replace({'REDACTED': None},inplace =True)
+
+    df = pd.merge(trr_df, officer_df,  how='left', left_on=['officer_first_name', 'officer_last_name'],
+                       right_on=['first_name', 'last_name']).fillna("None")
+
+    df8 = df[df['id'] != 'None'].reset_index(drop=True)
     df4 = df[df['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
     df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
 
@@ -303,13 +303,12 @@ def integration(List):
     p4 = df3[df3['id'] != 'None'].reset_index(drop=True)
     p5 = df3[df3['id'] == 'None'].reset_index(drop=True)
 
-    final_trr_status_cleaned = pd.concat([df6, df6, p3, p4, p5])
+    final_trr_status_cleaned = pd.concat([df8, df6, p3, p4, p5])
     final_trr_status_cleaned = final_trr_status_cleaned[['officer_rank', 'officer_star', 'status', 'status_datetime', 'officer_age', 'officer_unit_at_incident',
          'trr_report_id','id']]
 
-    final_trr_status_cleaned.to_csv('final_trr_status_cleaned.csv')
+    final_trr_status_cleaned.to_csv('merge2.csv')
 
-    #3.1 trr_refresh starts here
     trr_df = pd.read_csv(List[0])
     officer_df = pd.read_csv(List[1])
     trr_df = trr_df.rename(columns={'id': 'trr_id'})
@@ -318,9 +317,9 @@ def integration(List):
     #'officer_first_name', 'officer_middle_initial', 'officer_last_name', 'officer_middle_initial', 'officer_appointed_date', 'officer_birth_year', 'officer_gender', 'officer_race'],
     #'first_name','middle_initial','last_name','suffix_name','appointed_date','birth_year','gender','race'])
     df = pd.merge(trr_df, officer_df, how='left',
-                  left_on=['officer_first_name', 'officer_middle_initial','officer_last_name','officer_appointed_date', 'officer_gender'
+                  left_on=['officer_first_name','officer_last_name'
                            ],
-                  right_on=['first_name', 'middle_initial','last_name','appointed_date','gender']).fillna("None")
+                  right_on=['first_name','last_name']).fillna("None")
     df9 = df[df['id'] != 'None'].reset_index(drop=True)
     df4 = df[df['id'] == 'None'][trr_df.columns.values.tolist()].reset_index(drop=True)
     df4['officer_birth_year'] = df4['officer_birth_year'].apply(pd.to_numeric, errors='coerce')
@@ -353,7 +352,7 @@ def integration(List):
     p5 = df3[df3['id'] == 'None'].reset_index(drop=True)
 
     final_trr_cleaned = pd.concat([df9, df6, p3, p4, p5])
-    final_trr_cleaned = final_trr_cleaned[['trr_id', 'event_number', 'cr_number', 'beat', 'block', 'direction',
+    final_trr_cleaned = final_trr_cleaned[['trr_id','id', 'event_number', 'cr_number', 'beat', 'block', 'direction',
                                            'street', 'location', 'trr_datetime',
                                            'indoor_or_outdoor', 'lighting_condition', 'weather_condition',
                                            'notify_oemc', 'notify_district_sergeant', 'notify_op_command',
@@ -365,10 +364,9 @@ def integration(List):
                                            'officer_unit_name', 'officer_unit_detail',
                                            'trr_created', 'latitude', 'longitude', 'point']]
     final_trr_cleaned = final_trr_cleaned.reset_index(drop=True)
-    final_trr_cleaned.to_csv('final_trr_cleaned.csv')
-
-    #3.2 Starts here
+    final_trr_cleaned.to_csv('merge3.csv')
     unit_info=pd.read_csv('csv/original/postgres_public_data_policeunit.csv')
+
     for i in range(len(final_trr_cleaned['officer_unit_name'])):
         print("Working")
         if len(final_trr_cleaned['officer_unit_name'][i]) == 3:
@@ -380,7 +378,7 @@ def integration(List):
 
     new_df_unit_id = pd.merge(final_trr_cleaned, unit_info, how='left', left_on='officer_unit_name',
                               right_on='unit_name').fillna("None")
-
+    print(new_df_unit_id.columns)
     new_df_unit_id = new_df_unit_id[['trr_id', 'event_number', 'cr_number', 'beat', 'block', 'direction',
                                      'street', 'location', 'trr_datetime', 'indoor_or_outdoor',
                                      'lighting_condition', 'weather_condition', 'notify_oemc',
@@ -390,9 +388,9 @@ def integration(List):
                                      'subject_armed', 'subject_injured', 'subject_alleged_injury',
                                      'subject_age', 'subject_birth_year', 'subject_gender', 'subject_race',
                                      'officer_age', 'officer_unit_name', 'officer_unit_detail',
-                                     'trr_created', 'latitude', 'longitude', 'point', 'id', 'description']]
+                                     'trr_created', 'latitude', 'longitude', 'point', 'id_x', 'description']]
     new_df_unit_id = new_df_unit_id.rename(columns={'id': 'officer_unit_id', 'description': 'officer_unit_detail_id'})
-    new_df_unit_id.to_csv('merge5.csv')
+    new_df_unit_id.to_csv('merge4.csv')
 
 if __name__ == '__main__':
     #Type correct after OpenRefine
