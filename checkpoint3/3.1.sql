6603944f381fdefd9022a9d75180c8d74ac83498
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-09 14:11:14 -0600

    Update 3.1.sql

--- checkpoint3/3.1.sql
+++ checkpoint3/3.1.sql
@@ -4,7 +4,7 @@ FROM data_allegationcategory
 WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
 OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E');
 
---allegation ids from category ids of DAM
+--allegation ids from category ids of DAM -> some allegations have multiple associated category id's
 SELECT allegation_id
 FROM data_officerallegation
 WHERE data_officerallegation.allegation_category_id IN
@@ -13,68 +13,41 @@ WHERE data_officerallegation.allegation_category_id IN
   WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
   OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'));
 
---beat id from allegation id's from DAM categories
-SELECT beat_id
-FROM data_allegation
-WHERE beat_id IS NOT NULL AND data_allegation.crid IN
-  (SELECT allegation_id
-  FROM data_officerallegation
-  WHERE data_officerallegation.allegation_category_id IN
-    (SELECT id
-    FROM data_allegationcategory
-    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-    OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')));
-
---id's from areas with those given beat id's
-SELECT id
-FROM data_area
-WHERE data_area.id IN
-  (SELECT beat_id
-  FROM data_allegation
-  WHERE beat_id IS NOT NULL AND data_allegation.crid IN
-    (SELECT allegation_id
-    FROM data_officerallegation
-    WHERE data_officerallegation.allegation_category_id IN
-      (SELECT id
-      FROM data_allegationcategory
-      WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-      OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))));
-
---Race, count for those given id's
-SELECT race, count
-FROM data_racepopulation
-WHERE data_racepopulation.id IN
-  (SELECT id
-  FROM data_area
-  WHERE data_area.id IN
-    (SELECT beat_id
-    FROM data_allegation
-    WHERE beat_id IS NOT NULL AND data_allegation.crid IN
+--area_ids mapping from those allegation id's
+SELECT area_id
+FROM data_allegation_areas
+WHERE data_allegation_areas.allegation_id IN
       (SELECT allegation_id
       FROM data_officerallegation
       WHERE data_officerallegation.allegation_category_id IN
         (SELECT id
         FROM data_allegationcategory
         WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-        OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')))));
+        OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')));
+
+
+--Race, count for those given id's -> THIS NEEDS TO BE FIXED
+SELECT race, count, area_id
+FROM data_racepopulation
+WHERE data_racepopulation.id IN
+  (SELECT area_id
+  FROM data_allegation_areas
+  WHERE data_allegation_areas.allegation_id IN
+        (SELECT allegation_id
+        FROM data_officerallegation
+        WHERE data_officerallegation.allegation_category_id IN
+          (SELECT id
+          FROM data_allegationcategory
+          WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+          OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')));
 
 --Geoms for all these id's
-SELECT id,geom
+SELECT geom
 FROM data_linearea
 WHERE data_linearea.id IN
-  (SELECT id
-  FROM data_area
-  WHERE data_area.id
-  IN
-    (SELECT beat_id
-    FROM data_allegation
-    WHERE beat_id IS NOT NULL AND data_allegation.crid
-    IN
-      (SELECT allegation_id
-      FROM data_officerallegation
-      WHERE data_officerallegation.allegation_category_id
-      IN
-        (SELECT id
-        FROM data_allegationcategory
-        WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-        OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')))));
+
+--polygon as string ->
+SELECT st_astext(polygon)
+FROM data_area
+WHERE data_area.id
+IN
