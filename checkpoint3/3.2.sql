433eb42885ea6cc2bfa3e3ac7b6cf0be7d7a9447
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-09 15:13:29 -0600

    updated code

--- checkpoint3/3.2.sql
+++ checkpoint3/3.2.sql
@@ -1,32 +1,7 @@
---Gives all officers who have DAM allegations
-SELECT DISTINCT officer_id
-FROM data_officerallegation
-WHERE data_officerallegation.allegation_category_id IN
-  (SELECT id
-  FROM data_allegationcategory
-  WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-  OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'));
-
---Race, gender, age related to DAM allegations
-SELECT race, gender, age
-FROM data_complainant
-WHERE data_complainant.allegation_id
-IN
-  (SELECT allegation_id
-  FROM data_officerallegation
-  WHERE data_officerallegation.allegation_category_id
-  IN
-    (SELECT id
-    FROM data_allegationcategory
-    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-    OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))));
-
-
---officer id's involved in these complaints
+--officer id's involved in DAM complaints
 SELECT officer_id
 FROM data_officerallegation
-WHERE data_officerallegation.allegation_category_id
-IN
+WHERE data_officerallegation.allegation_category_id IN
   (SELECT id
   FROM data_allegationcategory
   WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
@@ -37,6 +12,19 @@ SELECT salary, pay_grade
 FROM data_salary
 WHERE data_salary.officer_id IN
   (SELECT officer_id
+  FROM data_officerallegation
+  WHERE data_officerallegation.allegation_category_id
+  IN
+    (SELECT id
+    FROM data_allegationcategory
+    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+    OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')));
+
+--race, gender, composition
+SELECT gender, race, birth_year
+FROM data_complainant
+WHERE data_complainant.allegation_id IN
+  (SELECT allegation_id
   FROM data_officerallegation
   WHERE data_officerallegation.allegation_category_id
   IN
