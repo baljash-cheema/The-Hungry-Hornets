007aa6261fbb90a2b95153a1cc1714d79aa324fc
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-09 07:50:58 -0600

    potential code for heatmap

--- checkpoint3/3.1.sql
+++ checkpoint3/3.1.sql
@@ -57,3 +57,24 @@ WHERE data_racepopulation.id IN
         FROM data_allegationcategory
         WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
         OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')))));
+
+--Geoms for all these id's 
+SELECT id,geom
+FROM data_linearea
+WHERE data_linearea.id IN
+  (SELECT id
+  FROM data_area
+  WHERE data_area.id
+  IN
+    (SELECT beat_id
+    FROM data_allegation
+    WHERE beat_id IS NOT NULL AND data_allegation.crid
+    IN
+      (SELECT allegation_id
+      FROM data_officerallegation
+      WHERE data_officerallegation.allegation_category_id
+      IN
+        (SELECT id
+        FROM data_allegationcategory
+        WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+        OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')))));
