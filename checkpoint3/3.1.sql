adff00f6ea45578b661646a6b5c2126e2df24d5e
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-09 14:57:51 -0600

    updated queries

--- checkpoint3/3.1.sql
+++ checkpoint3/3.1.sql
@@ -4,8 +4,8 @@ FROM data_allegationcategory
 WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
 OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E');
 
---allegation ids from category ids of DAM -> some allegations have multiple associated category id's
-SELECT allegation_id
+--distinct allegation ids from category ids of DAM -> 1267
+SELECT DISTINCT(allegation_id)
 FROM data_officerallegation
 WHERE data_officerallegation.allegation_category_id IN
   (SELECT id
@@ -13,11 +13,11 @@ WHERE data_officerallegation.allegation_category_id IN
   WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
   OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'));
 
---area_ids mapping from those allegation id's -> 5293 of these
+--area_ids from those allegation id's -> 5293 of these (464 if area_id distinct)
 SELECT area_id
 FROM data_allegation_areas
 WHERE data_allegation_areas.allegation_id IN
-  (SELECT allegation_id
+  (SELECT DISTINCT(allegation_id)
   FROM data_officerallegation
   WHERE data_officerallegation.allegation_category_id IN
     (SELECT id
@@ -25,14 +25,14 @@ WHERE data_allegation_areas.allegation_id IN
     WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
     OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')));
 
---polygons as strings for those area_ids (but we lost count)
+--polygons as strings for those area_ids -> 464
 SELECT st_astext(polygon)
 FROM data_area
 WHERE data_area.id IN
   (SELECT area_id
   FROM data_allegation_areas
   WHERE data_allegation_areas.allegation_id IN
-    (SELECT allegation_id
+    (SELECT DISTINCT(allegation_id)
     FROM data_officerallegation
     WHERE data_officerallegation.allegation_category_id IN
       (SELECT id
@@ -40,14 +40,14 @@ WHERE data_area.id IN
       WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
       OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))));
 
---Race, count for those given id's
+--Race, count for those given id's -> 497
 SELECT race, count, area_id
 FROM data_racepopulation
 WHERE data_racepopulation.area_id IN
   (SELECT area_id
   FROM data_allegation_areas
   WHERE data_allegation_areas.allegation_id IN
-    (SELECT allegation_id
+    (SELECT DISTINCT(allegation_id)
     FROM data_officerallegation
     WHERE data_officerallegation.allegation_category_id IN
       (SELECT id
@@ -55,14 +55,14 @@ WHERE data_racepopulation.area_id IN
       WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
       OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))));
 
---Geoms for all these id's
+--Geoms for all these id's -> 98
 SELECT geom
 FROM data_linearea
 WHERE data_linearea.id IN
   (SELECT area_id
   FROM data_allegation_areas
   WHERE data_allegation_areas.allegation_id IN
-    (SELECT allegation_id
+    (SELECT DISTINCT(allegation_id)
     FROM data_officerallegation
     WHERE data_officerallegation.allegation_category_id IN
       (SELECT id
