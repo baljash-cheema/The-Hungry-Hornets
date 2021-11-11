8593e2cb26288564e528d0be604b77ecf7778d51
vikramNU <90998121+vikramNU@users.noreply.github.com>
2021-11-11 16:50:02 -0600

    Update 3.1.py

--- checkpoint3/src/3.1.py
+++ checkpoint3/src/3.1.py
@@ -8,17 +8,31 @@ import json
 f = open('Boundaries.geojson',)
 df0 = json.load(f)
 
-df1 = pd.read_csv("Result_63.csv")
+df1 = pd.read_csv("Result_85.csv")
+
+lt=[]
+#print(df1.head())
+for x in range(len(df1['race'])-4):
+    lt.append(str(df1['race'][x])+' - '+str(df1['people_count'][x])+', '+str(df1['race'][x+1])+' - '+str(df1['people_count'][x+1])+', '+str(df1['race'][x+2])+' - '+str(df1['people_count'][x+2])+', '+str(df1['race'][x+3])+' - '+str(df1['people_count'][x+3]))
+
+lt.append(' 1')
+lt.append(' 1')
+lt.append(' 1')
+lt.append(' 1')
+
+df1['racex']=lt
+df1 = df1.drop_duplicates(subset=['area_name'])
+df1 = df1.reset_index()
 
 for x in range(len(df0['features'])):
     y = df0['features'][x]['properties']['pri_neigh']
     for z in range(len(df1['area_name'])):
+
         if y==str(df1['area_name'][z]):
-            df0['features'][x]['properties']['pri_neigh'] = y + ', Number of allegation with Drug and alcohol abuse - ' +str(df1['count'][z])
+            df0['features'][x]['properties']['pri_neigh']=(y + ': Number of allegations with drug and alcohol abuse - ' +str(df1['count'][z])+ '; Demographics: ' +str(df1['racex'][z]))
 
-print(df0['features'][0]['properties']['pri_neigh'])
-print(df0['features'][1]['properties']['pri_neigh'])
-
+print(df0)
 #dfx=df0.to_json("R.geojson")
 with open('R.geojson', 'w') as json_file:
   json.dump(df0, json_file)
+
