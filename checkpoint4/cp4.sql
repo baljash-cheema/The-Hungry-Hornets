633d5f129a86ebcfc76c8f643b0eff794625542d
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-16 15:14:10 -0600

    tweaked it to above and below average

--- checkpoint4/cp4.sql
+++ checkpoint4/cp4.sql
@@ -33,10 +33,12 @@ JOIN da_cohort ON da_cohort.officer_id = class_example.src;
 Question 2:
 Among officers who have allegations against them of drug and alcohol abuse, does
 the pattern of co-accusuals with them and other officers vary, as related to the salary
-of a given officer? Specifically
+of a given officer? Specifically, does the co-accusual pattern change among officers
+who make above and below the average salary of all officers?
 
 */
 
+--Salary greater than average
 DROP TABLE IF EXISTS da_category_ids;
 CREATE TEMP TABLE da_category_ids AS (
     SELECT id
@@ -52,7 +54,35 @@ CREATE TEMP TABLE da_cohort AS (
 
 DELETE FROM da_cohort c
 USING data_salary s WHERE s.officer_id = c.officer_id
-AND s.salary < 60000;
+AND s.salary > (SELECT AVG(salary) FROM data_salary);
+
+DROP TABLE IF EXISTS class_example;
+CREATE TEMP TABLE class_example AS (
+SELECT da1.officer_id src, da2.officer_id dst, COUNT(DISTINCT da1.allegation_id) relationship
+FROM data_officerallegation da1
+JOIN data_officerallegation da2 ON da1.allegation_id = da2.allegation_id AND da1.officer_id < da2.officer_id
+GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
+
+SELECT * FROM class_example
+JOIN da_cohort ON da_cohort.officer_id = class_example.src;
+
+--Salary less than average
+DROP TABLE IF EXISTS da_category_ids;
+CREATE TEMP TABLE da_category_ids AS (
+    SELECT id
+    FROM data_allegationcategory
+    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+    OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'));
+
+DROP TABLE IF EXISTS da_cohort;
+CREATE TEMP TABLE da_cohort AS (
+    SELECT DISTINCT officer_id
+    FROM data_officerallegation
+    WHERE allegation_category_id IN (SELECT * from da_category_ids));
+
+DELETE FROM da_cohort c
+USING data_salary s WHERE s.officer_id = c.officer_id
+AND s.salary < (SELECT AVG(salary) FROM data_salary);
 
 DROP TABLE IF EXISTS class_example;
 CREATE TEMP TABLE class_example AS (
