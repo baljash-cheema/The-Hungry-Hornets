ce9af331e35a5362bc622380dbda5739ffe640b7
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-16 14:37:23 -0600

    udpate

--- checkpoint4/cp4.sql
+++ checkpoint4/cp4.sql
@@ -1,5 +1,29 @@
+-- CODE QUESITON 1
+DROP TABLE IF EXISTS da_category_ids;
+CREATE TEMP TABLE da_category_ids AS (
+    SELECT id
+    FROM data_allegationcategory
+    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+    OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E')
+);
 
---QUESITON 1
+DROP TABLE IF EXISTS da_cohort;
+CREATE TEMP TABLE da_cohort AS (
+    SELECT DISTINCT officer_id
+    FROM data_officerallegation
+    WHERE allegation_category_id IN (SELECT * from da_category_ids));
+
+DROP TABLE IF EXISTS class_example;
+CREATE TEMP TABLE class_example AS (
+SELECT da1.officer_id src, da2.officer_id dst, COUNT(DISTINCT da1.allegation_id) relationship
+FROM data_officerallegation da1
+JOIN data_officerallegation da2 ON da1.allegation_id = da2.allegation_id AND da1.officer_id < da2.officer_id
+GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
+
+SELECT * FROM class_example
+JOIN da_cohort ON da_cohort.officer_id = class_example.src;
+
+
 --officers with same allegation catergories id's
 
 SELECT da1.officer_id, da2.officer_id, da2.allegation_category_id
@@ -14,6 +38,21 @@ WHERE da2.allegation_category_id IN
 
 --condensed
 SELECT da1.officer_id, da2.officer_id, da2.allegation_category_id FROM data_officerallegation da1 JOIN data_officerallegation da2 ON da1.allegation_category_id = da2.allegation_category_id AND da1.officer_id < da2.officer_id WHERE da2.allegation_category_id IN (SELECT id FROM data_allegationcategory WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%' OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')) GROUP BY da1.officer_id, da2.officer_id, da2.allegation_category_id ORDER BY count(*) DESC;
+
+--THIS JOINS OFFICERS WHO HAVE DAM ALLEGATIONS
+
+SELECT da1.officer_id src, da2.officer_id dst, COUNT(DISTINCT da1.allegation_id) relationship
+FROM data_officerallegation da1
+JOIN data_officerallegation da2 ON da1.allegation_id = da2.allegation_id AND da1.officer_id < da2.officer_id
+WHERE da2.allegation_category_id IN
+    (SELECT id
+    FROM data_allegationcategory
+    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+    OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))
+GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC;
+
+--condensed to get on graphx
+SELECT da1.officer_id src, da2.officer_id dst, COUNT(DISTINCT da1.allegation_id) relationship FROM data_officerallegation da1 JOIN data_officerallegation da2 ON da1.allegation_id = da2.allegation_id AND da1.officer_id < da2.officer_id WHERE da2.allegation_category_id IN (SELECT id FROM data_allegationcategory WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%' OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E')) GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC;
 
 
 /*
