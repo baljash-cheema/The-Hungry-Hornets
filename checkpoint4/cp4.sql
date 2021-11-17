dc8d5e9ea259c859c5d786b5320e27a17dce726d
lbarsky <88569183+lbarsky@users.noreply.github.com>
2021-11-16 18:41:44 -0600

    awards yay!

--- checkpoint4/cp4.sql
+++ checkpoint4/cp4.sql
@@ -98,12 +98,13 @@ Specifically, how does the co-accusal pattern compare between those with more an
 */
 
 /* 
-Finding number of awards per officer
+Creating modified awards table with number of awards per officer
 */
 
-SELECT officer_id, count(award_type) as num_awards
-FROM data_award
-WHERE data_award.officer_id IN
+CREATE TEMP TABLE award_count AS (
+    SELECT officer_id offid, count(award_type) num_awards
+    FROM data_award
+    WHERE data_award.officer_id IN
                   (SELECT id
                   FROM data_officer
                   WHERE data_officer.id IN
@@ -114,9 +115,26 @@ WHERE data_award.officer_id IN
                       FROM data_allegationcategory
                       WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
                       OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))))
-GROUP BY officer_id;
+    group by officer_id);
 
-/*--Award number greater than average*/
+/* Award number greater than average */
+
+DROP TABLE IF EXISTS award_count;
+CREATE TEMP TABLE award_count AS (
+    SELECT officer_id offid, count(award_type) num_awards
+    FROM data_award
+    WHERE data_award.officer_id IN
+                  (SELECT id
+                  FROM data_officer
+                  WHERE data_officer.id IN
+                    (SELECT officer_id
+                    FROM data_officerallegation
+                    WHERE data_officerallegation.allegation_category_id IN
+                      (SELECT id
+                      FROM data_allegationcategory
+                      WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+                      OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))))
+    group by officer_id);
 
 DROP TABLE IF EXISTS da_category_ids;
 CREATE TEMP TABLE da_category_ids AS (
@@ -132,8 +150,54 @@ CREATE TEMP TABLE da_cohort AS (
     WHERE allegation_category_id IN (SELECT * from da_category_ids));
 
 DELETE FROM da_cohort c
-USING data_award a WHERE a.officer_id = c.officer_id
-AND count(a.award_type) > (SELECT AVG(count(award_type) FROM data_award);
+USING award_count a WHERE a.offid = c.officer_id
+AND a.num_awards > (SELECT AVG(num_awards) FROM award_count);
+
+DROP TABLE IF EXISTS class_example;
+CREATE TEMP TABLE class_example AS (
+SELECT da1.officer_id src, da2.officer_id dst, COUNT(DISTINCT da1.allegation_id) relationship
+FROM data_officerallegation da1
+JOIN data_officerallegation da2 ON da1.allegation_id = da2.allegation_id AND da1.officer_id < da2.officer_id
+GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
+
+SELECT * FROM class_example
+JOIN da_cohort ON da_cohort.officer_id = class_example.src;
+
+/* Award number below average */
+
+DROP TABLE IF EXISTS award_count;
+CREATE TEMP TABLE award_count AS (
+    SELECT officer_id offid, count(award_type) num_awards
+    FROM data_award
+    WHERE data_award.officer_id IN
+                  (SELECT id
+                  FROM data_officer
+                  WHERE data_officer.id IN
+                    (SELECT officer_id
+                    FROM data_officerallegation
+                    WHERE data_officerallegation.allegation_category_id IN
+                      (SELECT id
+                      FROM data_allegationcategory
+                      WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+                      OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))))
+    group by officer_id);
+
+DROP TABLE IF EXISTS da_category_ids;
+CREATE TEMP TABLE da_category_ids AS (
+    SELECT id
+    FROM data_allegationcategory
+    WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
+    OR data_allegationcategory.category_code IN ('08J', '024', '003', '003A', '003B', '003C', '003D', '003E'));
+
+DROP TABLE IF EXISTS da_cohort;
+CREATE TEMP TABLE da_cohort AS (
+    SELECT DISTINCT officer_id
+    FROM data_officerallegation
+    WHERE allegation_category_id IN (SELECT * from da_category_ids));
+
+DELETE FROM da_cohort c
+USING award_count a WHERE a.offid = c.officer_id
+AND a.num_awards < (SELECT AVG(num_awards) FROM award_count);
 
 DROP TABLE IF EXISTS class_example;
 CREATE TEMP TABLE class_example AS (
