9fd34582cec8e13634c6e8cd292f3e60d6189944
baljash-cheema <baljash-cheema@northwestern.edu>
2021-11-17 09:56:55 -0600

    Update cp4.sql

--- checkpoint4/cp4.sql
+++ checkpoint4/cp4.sql
@@ -1,9 +1,11 @@
 /*
 Question 1:
 Among officers who have drug and alcohol abuse or medical allegations against them,
-how often are they co-accused? What is the pattern and/or relationship of this?
+how often are they co-accused? How does this co-accusal pattern vary based on whether
+officers earn below or above the average annual salary across the full department?
 */
 
+--Co-accusals of all officers with DAM allegations
 DROP TABLE IF EXISTS da_category_ids;
 CREATE TEMP TABLE da_category_ids AS (
     SELECT id
@@ -27,14 +29,7 @@ GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
 SELECT src,dst,relationship FROM class_example
 JOIN da_cohort ON da_cohort.officer_id = class_example.src;
 
-/*
-Question 2:
-Among officers who have drug and alcohol abuse or medical allegations against them,
-does the pattern of those co-accused differ among officers who earn above or below the
-average annual salary within the department, respectively?
-*/
-
---Salary greater than average
+--Co-accusals for officers with DAM allegations earning above average salary
 DROP TABLE IF EXISTS da_category_ids;
 CREATE TEMP TABLE da_category_ids AS (
     SELECT id
@@ -62,7 +57,7 @@ GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
 SELECT src,dst,relationship FROM class_example
 JOIN da_cohort ON da_cohort.officer_id = class_example.src;
 
---Salary less than average
+--Co-accusals for officers with DAM allegations earning above average salary
 DROP TABLE IF EXISTS da_category_ids;
 CREATE TEMP TABLE da_category_ids AS (
     SELECT id
@@ -91,34 +86,14 @@ SELECT src,dst,relationship FROM class_example
 JOIN da_cohort ON da_cohort.officer_id = class_example.src;
 
 /*
-Question 3:
+Question 2:
 Among officers who have drug & alcohol abuse and medical allegations against them,
 how does the co-accusal pattern compare between more and less decorated officers?
-Specifically, how does the co-accusal pattern compare between those with more and less than the average number of awards, respectively?
+Specifically, how does the co-accusal pattern compare between those with more and
+less than the average number of award types won, respectively?
 */
 
-/*
-Creating modified awards table with number of awards per officer
-*/
-
-CREATE TEMP TABLE award_count AS (
-    SELECT officer_id offid, count(award_type) num_awards
-    FROM data_award
-    WHERE data_award.officer_id IN
-                  (SELECT id
-                  FROM data_officer
-                  WHERE data_officer.id IN
-                    (SELECT officer_id
-                    FROM data_officerallegation
-                    WHERE data_officerallegation.allegation_category_id IN
-                      (SELECT id
-                      FROM data_allegationcategory
-                      WHERE data_allegationcategory.category = 'Drug / Alcohol Abuse' OR data_allegationcategory.category = 'Medical' or allegation_name LIKE 'Medical Roll%'
-                      OR data_allegationcategory.category_code IN ('024', '003', '003A', '003B', '003C', '003D', '003E'))))
-    group by officer_id);
-
-/* Award number greater than average */
-
+--Co-accusals of officers with DAM allegations earning more than average distinct award types
 DROP TABLE IF EXISTS award_count;
 CREATE TEMP TABLE award_count AS (
     SELECT officer_id offid, count(award_type) num_awards
@@ -163,8 +138,7 @@ GROUP BY da1.officer_id, da2.officer_id ORDER BY count(*) DESC);
 SELECT src,dst,relationship FROM class_example
 JOIN da_cohort ON da_cohort.officer_id = class_example.src;
 
-/* Award number below average */
-
+--Co-accusals of officers with DAM allegations earning less than average distinct award types
 DROP TABLE IF EXISTS award_count;
 CREATE TEMP TABLE award_count AS (
     SELECT officer_id offid, count(award_type) num_awards
